78a96879c166fc52c67d1eac14ae640966c6805e
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2025-10-04 15:19:48 -0700

    Attempt to fix github and linkedin img icon error

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ const viewPDF = document.querySelector("#pdf-btn");
 viewPDF.addEventListener("click", () => {
   window.open("downloads/Kevin-Sandoval-Resume.pdf", "_blank");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  // LinkedIn click
+  const linkedinIcon = document.getElementById("linkedin-icon");
+  linkedinIcon.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/kevinrsandoval", "_blank");
+  });
+
+  // GitHub click
+  const githubIcon = document.getElementById("github-icon");
+  githubIcon.addEventListener("click", () => {
+    window.open("https://github.com/KevinSandoval12", "_blank");
+  });
+});
